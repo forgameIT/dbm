df76bd6c89e8d063296321e2f35cd87ce562c0d6
梁龙 <lianglong@forgame.com>
2018-10-25 18:07:22 +0800

    优化

--- mysql-rsync/mysql-exec.sh
+++ mysql-rsync/mysql-exec.sh
@@ -24,8 +24,8 @@ function_update()
             touch ${FILE_PATH}/.update.history.log
         fi
         if [ -z `cat ${FILE_PATH}/.update.history.log|grep $SQLHASH` ]; then
-            mysql -h"${CONFIG_MYSQL_HOST}" -u"${CONFIG_MYSQL_USER}" -p"${CONFIG_MYSQL_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS ${CONFIG_MYSQL_DATABASE} DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci"
-            mysql -h"${CONFIG_MYSQL_HOST}" -u"${CONFIG_MYSQL_USER}" -p"${CONFIG_MYSQL_PASSWORD}" -D${CONFIG_MYSQL_DATABASE} < ${FILE_PATH}/update.sql
+            mysql -h"${CONFIG_MYSQL_HOST}" -u"${CONFIG_MYSQL_USER}" -p"${CONFIG_MYSQL_PASSWORD}" -P"${CONFIG_MYSQL_PORT}" -e "CREATE DATABASE IF NOT EXISTS ${CONFIG_MYSQL_DATABASE} DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci"
+            mysql -h"${CONFIG_MYSQL_HOST}" -u"${CONFIG_MYSQL_USER}" -p"${CONFIG_MYSQL_PASSWORD}" -P"${CONFIG_MYSQL_PORT}" -D${CONFIG_MYSQL_DATABASE} < ${FILE_PATH}/update.sql
             rm -f ${FILE_PATH}/update.sql
             echo "$SQLHASH" >> "${FILE_PATH}/.update.history.log"
         fi
@@ -38,7 +38,7 @@ function_update()
 function_backup()
 {
     command -v mysqldump >/dev/null 2>&1 ||  (apt-get update && apt-get install -y mysql-client)
-    mysqldump -h"${CONFIG_MYSQL_HOST}" -u"${CONFIG_MYSQL_USER}" -p"${CONFIG_MYSQL_PASSWORD}" --skip-lock-tables --add-drop-database --add-drop-table --databases ${CONFIG_MYSQL_DATABASE} > ${FILE_PATH}/latest.sql
+    mysqldump -h"${CONFIG_MYSQL_HOST}" -u"${CONFIG_MYSQL_USER}" -p"${CONFIG_MYSQL_PASSWORD}" -P"${CONFIG_MYSQL_PORT}" --skip-lock-tables --add-drop-database --add-drop-table --databases ${CONFIG_MYSQL_DATABASE} > ${FILE_PATH}/latest.sql
     if [ -n "$VERSION_NUMBER" ]; then
         /bin/cp ${FILE_PATH}/latest.sql ${FILE_PATH}/${VERSION_NUMBER}_latest.sql
         /bin/cp ${FILE_PATH}/latest.sql ${FILE_PATH}/${VERSION_NUMBER}_`date "+%Y%m%d%H%M%S"`.sql
